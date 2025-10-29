a64bc51f570848091617310deaa1ca53609d6ea1
h3mehsen <h3mehsen@enib.fr>
2025-10-29 01:17:04 +0100

    fix: change condition to prevent API retry when empty but successful response and add logs

--- src/el/el_jobs/company_profile_el_job.py
+++ src/el/el_jobs/company_profile_el_job.py
@@ -11,19 +11,19 @@ class CompanyProfileELJob(ExtractLoadPipeline):
     def __init__(self):
         super().__init__(source="FMP-Company Profile", table=company_profile)
 
-    def run(self):
-        """Main execution method for extracting and loading company profiles."""
+    def run(self) -> None:
+        """Main execution method for extracting and loading company profiles data"""
         all_data = []
 
-        self._log(header="EXTRACTING Start - endpoint : Company Profile")
+        self._log(header="EXTRACTING Start - endpoint: Company Profile")
         self._log(message="Starting batch extraction...", phase="EXTRACT")
 
         for symbol in self.available_companies:
-            attempt = 1
+
             data = self.extract(endpoint="/profile", query=f"symbol={symbol}")
 
-            if not data:
-                while attempt <= self.MAX_RETRIES:
+            if not data and self.status_code != 200:
+                for attempt in range(1, self.MAX_RETRIES + 1):
                     self._log(
                         message=f"Retry {attempt}/{self.MAX_RETRIES} - failed to extract company profile for {symbol}. Retrying in {2**attempt}s...",
                         phase="EXTRACT",
@@ -32,19 +32,17 @@ class CompanyProfileELJob(ExtractLoadPipeline):
                     data = self.extract(endpoint="/profile", query=f"symbol={symbol}")
                     if data:
                         break
-                    attempt += 1
-
             if data:
-                all_data.append(data)
+                all_data.extend(data)
 
-        self._log(header="EXTRACTING End - endpoint : Company Profile")
+        self._log(header="EXTRACTING End - endpoint: Company Profile")
 
-        self._log(header="LOADING Start - endpoint : Company Profile")
+        self._log(header="LOADING Start - endpoint: Company Profile")
+
         if all_data:
-            self._log(message=f"Batch size: {len(all_data)} symbols collected. Starting load...", phase="LOAD")
             self.load(all_data)
-
+            self._log(message=f"Batch size: {len(all_data)} companies profile.", phase="LOAD")
         else:
             self._log(message="No data collected.", phase="LOAD")
 
-        self._log(header="LOADING End - endpoint : Company Profile")
+        self._log(header="LOADING End - endpoint: Company Profile")
