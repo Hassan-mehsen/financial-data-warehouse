6e2b8e509351ef8a6aed100768b566859ff6d5dc
h3mehsen <h3mehsen@enib.fr>
2025-10-31 00:35:22 +0100

    feat(staging): add stg_earnings_reports model for earnings data normalization

---
+++ dbt_financial_market/models/staging/stg_earnings_reports.sql
@@ -0,0 +1,27 @@
+WITH extend_json AS(
+
+    SELECT source,
+           ingestion_ts,
+           jsonb_array_elements(json_list::JSONB) AS elem
+    FROM {{ source('raw','earnings_reports') }}
+
+),
+
+renamed_and_typed AS (
+    SELECT
+       NULLIF( (elem->>'symbol'), '')::VARCHAR(50)                        AS symbol,
+       NULLIF( (elem->>'date'), '')::date                                 AS date,
+       TO_CHAR(NULLIF((elem->>'date'), '')::date, 'YYYYMMDD')::integer    AS date_key,
+       NULLIF( (elem->>'epsActual'), '')::numeric(10,6)                   AS eps_actual,
+       NULLIF( (elem->>'epsEstimated'), '')::numeric(10,6)                AS eps_estimated,
+       NULLIF( (elem->>'revenueActual'), '')::numeric(20,2)               AS revenue_actual,
+       NULLIF( (elem->>'revenueEstimated'), '')::numeric(20,2)            AS revenue_estimated,
+       NULLIF( (elem->>'lastUpdated'), '')::date                          AS last_updated,
+       source,
+       ingestion_ts
+    FROM extend_json
+)
+
+SELECT *
+FROM renamed_and_typed
+WHERE date_key IS NOT NULL AND symbol IS NOT NULL
