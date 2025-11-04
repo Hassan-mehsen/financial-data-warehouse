b3b42afa72c043cb39133d7d15f34a33f11e10ae
h3mehsen <h3mehsen@enib.fr>
2025-11-04 14:44:25 +0100

    feat(staging): create standardized date dimension

---
+++ dbt_financial_market/models/staging/common/stg_dim_date.sql
@@ -0,0 +1,18 @@
+
+WITH typed as (
+
+  SELECT
+
+    date_key::INTEGER       AS date_key,
+    date::DATE              AS date,
+    month::INTEGER          AS month,
+    year::INTEGER           AS year,
+    fiscal_quarter::INTEGER AS fiscal_quarter,
+    day_of_week::INTEGER    AS day_of_week,
+    is_weekend::BOOLEAN     AS is_weekend
+
+  FROM {{ ref('dates') }}  -- seeds
+
+)
+
+SELECT * FROM typed
