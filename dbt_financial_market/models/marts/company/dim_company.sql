097e5a46d2549e05e4b1d87decfa066894072c37
h3mehsen <h3mehsen@enib.fr>
2025-11-06 10:12:48 +0100

    feat(marts): add dim_company model defining the company dimension

---
+++ dbt_financial_market/models/marts/company/dim_company.sql
@@ -0,0 +1,35 @@
+
+SELECT 
+        map.company_id AS id,
+        scp.symbol,
+        scp.company_name,
+        scp.cik,
+        scp.isin,
+        scp.cusip,
+        scp.exchange_full_name,
+        scp.exchange_code,
+        scp.reported_currency,
+        scp.sector,
+        scp.industry,
+        scp.country_code,
+        scp.address_line,
+        scp.city,
+        scp.state_code,
+        scp.postal_code,
+        scp.phone_number,
+        scp.ceo_name,
+        scp.website_url,
+        scp.logo_url,
+        scp.full_time_employees,
+        scp.is_actively_trading,
+        scp.ipo_date,
+        scp.is_etf,
+        scp.is_adr,
+        scp.is_fund,
+
+        CURRENT_TIMESTAMP AS updated_at
+        
+FROM {{ ref('stg_company_profile') }} AS scp
+JOIN {{ ref('company_id_map') }}  AS map
+ ON scp.symbol = map.symbol
+
