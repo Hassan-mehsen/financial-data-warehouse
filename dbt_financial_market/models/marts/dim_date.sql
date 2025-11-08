9cf2864a648258538fe1f97245e96f8e0867aa7d
h3mehsen <h3mehsen@enib.fr>
2025-11-08 15:18:30 +0100

    feat(marts): add schema protection and index creation to dim_date

--- dbt_financial_market/models/marts/dim_date.sql
+++ dbt_financial_market/models/marts/dim_date.sql
@@ -1,2 +1,10 @@
+{{ 
+   config(  
+            on_schema_change='fail',
+            on_configuration_change = 'fail',
+            post_hook = create_index('date_key')
+        )
+}}
+
 SELECT *
 FROM {{ ref('stg_dim_date') }}
